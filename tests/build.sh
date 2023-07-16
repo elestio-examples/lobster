a2455c1822722a7067d45f8c362099fa5f9214e6
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-07-16 13:16:35 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,1 +1,5 @@
+sed -i "s~<nobody@#{Rails.application.domain}>~ENV.fetch("SMTP_SENDER", "")~g" ./app/mailers/ban_notification.rb
+sed -i "s~<nobody@#{Rails.application.domain}>~ENV.fetch("SMTP_SENDER", "")~g" ./app/mailers/application_mailer.rb
+sed -i "s~<nobody@#{Rails.application.domain}>~ENV.fetch("SMTP_SENDER", "")~g" ./script/mail_new_activity.rb
+
 docker buildx build . --output type=docker,name=elestio4test/lobster:latest | docker load
