26a204831e087382aa4a5b2e612e40ced61a4e00
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-07-17 15:57:53 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,12 +3,16 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for the software to be ready..."
 sleep 90s;
 
-docker-compose exec -T lobsters bash << EOF
+docker-compose exec -T lobster bash << EOF
 rails console
 user = User.find_by(username: "test")
 user.password = "${ADMIN_PASSWORD}"
 user.password_confirmation = "${ADMIN_PASSWORD}"
 user.email = "${ADMIN_EMAIL}"
-user.username = "admin"
-user.save
+user.save!
+EOF
+
+docker-compose exec -it lobster-db bash << EOF
+mysql -u root lobster -p${ADMIN_PASSWORD};
+UPDATE users SET username = 'admin' WHERE id = 2;
 EOF
