11beb7c51b4e76963b85d716974f9ba0e07296a2
Prem chavhan <pchavhan@logiquad.com>
2023-10-27 13:13:07 +0530

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,1 +1,7 @@
+rm -f Dockerfile
+rm -f docker-compose.yml
+rm -f docker-entrypoint.sh
+mv Dockerfile.new Dockerfile
+mv docker-compose-new.yml docker-compose.yml
+mv docker-entrypoint-new.sh docker-entrypoint.sh
 docker buildx build . --output type=docker,name=elestio4test/lobster:latest | docker load
