86486023134dff35def3190b02033837dd4b83b8
Prem Chavhan <pchavhan@logiquad.com>
2024-01-03 18:41:23 +0530

    Updated fixes

--- tests/build.sh
+++ tests/build.sh
@@ -1,6 +1,7 @@
-rm -f Dockerfile
-rm -f docker-compose.yml
-rm -f docker-entrypoint.sh
+#!/usr/bin/env bash
+# rm -f Dockerfile
+# rm -f docker-compose.yml
+# rm -f docker-entrypoint.sh
 mv Dockerfile.new Dockerfile
 mv docker-compose-new.yml docker-compose.yml
 mv docker-entrypoint-new.sh docker-entrypoint.sh
