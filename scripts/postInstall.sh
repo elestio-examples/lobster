9c6a6b3c1d6ab3c214d268e3ba6c1592f2e5789c
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-10-27 12:24:44 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,6 +3,11 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for the software to be ready..."
 sleep 90s;
 
+
+if [ -e "./initialized" ]; then
+    echo "Already initialized, skipping..."
+else
+
 docker-compose exec -T lobster bash << EOF
 rails console
 user = User.find_by(username: "test")
@@ -12,3 +17,9 @@ user.email = "${ADMIN_EMAIL}"
 user.username = "master"
 user.save!
 EOF
+
+sed -i 's@location / {@location /about {\nreturn 301 /;\n}\nlocation / {@g' /opt/elestio/nginx/conf.d/${DOMAIN}.conf
+
+docker exec elestio-nginx nginx -s reload;
+touch "./initialized"
+fi
